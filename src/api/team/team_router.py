883087d74ef8f947950e99d135df7f2e03006f20
Normaltic <normaltic1018@gmail.com>
2024-08-16 13:12:14 +0900

    Add status Code and tags

--- src/api/team/team_router.py
+++ src/api/team/team_router.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, status
 from typing import List
 from sqlalchemy.orm import Session
 
@@ -9,22 +9,22 @@ router = APIRouter(
     prefix="/api/team",
 )
 
-@router.get("/list", response_model=List[team_schema.Team])
+@router.get("/list", response_model=List[team_schema.Team], status_code=status.HTTP_200_OK, tags=["Team"])
 def team_list(db: Session = Depends(get_db)):
     _team_list = team_crud.get_team_list(db)
     return _team_list
 
-@router.get("/detail/{team_id}", response_model=team_schema.Team)
+@router.get("/detail/{team_id}", response_model=team_schema.Team, status_code=status.HTTP_200_OK, tags=["Team"])
 def team_detail(team_id: int, db: Session = Depends(get_db)):
     team = team_crud.get_team(db, team_id)
     return team
 
-@router.post("/create", response_model=team_schema.TeamCreate)
+@router.post("/create", response_model=team_schema.TeamCreate, status_code=status.HTTP_201_CREATED, tags=["Team"])
 def team_create(_team_create: team_schema.TeamCreate, db: Session = Depends(get_db)):
     team_crud.create_team(db, team_create=_team_create)
     return _team_create
 
-@router.delete("/delete", response_model=team_schema.TeamDelete)
+@router.delete("/delete", response_model=team_schema.TeamDelete, status_code=status.HTTP_202_ACCEPTED, tags=["Team"])
 def team_delete(_team_delete: team_schema.TeamDelete,
                 db: Session = Depends(get_db)):
     db_team = team_crud.get_team(db, team_id=_team_delete.team_id)
@@ -34,7 +34,7 @@ def team_delete(_team_delete: team_schema.TeamDelete,
     team_crud.delete_question(db, db_team=db_team)
     return _team_delete
 
-@router.put("/update", response_model=team_schema.TeamUpdate)
+@router.put("/update", response_model=team_schema.TeamUpdate, status_code=status.HTTP_202_ACCEPTED, tags=["Team"])
 def team_update(_team_update: team_schema.TeamUpdate,
                 db: Session = Depends(get_db)):
     db_team = team_crud.get_team(db, team_id=_team_update.team_id)
