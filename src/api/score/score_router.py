79867eb94033eebb76b1279a142bc7b1a8d81bd9
Normaltic <normaltic1018@gmail.com>
2024-08-16 14:06:11 +0900

    Add Score Tags

--- src/api/score/score_router.py
+++ src/api/score/score_router.py
@@ -12,12 +12,12 @@ router = APIRouter(
 )
 
 @router.get("/list", response_model=List[score_schema.Score], 
-            status_code=status.HTTP_200_OK)
+            status_code=status.HTTP_200_OK, tags=["Score"])
 def score_list(db: Session = Depends(get_db)):
     _score_list = score_crud.get_score_list(db)
     return _score_list
 
-@router.post("/addScore/{team_id}", status_code=status.HTTP_204_NO_CONTENT)
+@router.post("/addScore/{team_id}", status_code=status.HTTP_204_NO_CONTENT, tags=["Score"])
 def score_create(team_id: int, _score_create: score_schema.ScoreCreate,
                  db: Session = Depends(get_db)):
     team = team_crud.get_team(db, team_id=team_id)
@@ -27,7 +27,7 @@ def score_create(team_id: int, _score_create: score_schema.ScoreCreate,
                             score_create=_score_create)
 
 @router.delete("/delete" , status_code=status.HTTP_202_ACCEPTED, 
-               response_model=score_schema.Score)
+               response_model=score_schema.Score, tags=["Score"])
 def score_delete(_score_delete: score_schema.ScoreDelete,
                  db: Session = Depends(get_db)):
     db_score = score_crud.get_score(db, score_id=_score_delete.id)
@@ -38,6 +38,6 @@ def score_delete(_score_delete: score_schema.ScoreDelete,
     score_crud.delete_scroe(db, db_score=db_score)
     return _score_delete
 
-@router.delete("/resetScore", status_code=status.HTTP_204_NO_CONTENT)
+@router.delete("/resetScore", status_code=status.HTTP_204_NO_CONTENT, tags=["Score"])
 def score_reset(db: Session = Depends(get_db)):
     score_crud.reset_score(db)
